b169f6f068d9c53b860b764154b60cd7aea9a07b
Swampen <ole-ma@outlook.com>
2022-12-06 06:53:08 +0100

    ⭐

---
+++ day06/ape.py
@@ -0,0 +1,23 @@
+def get_input(file):
+    return open(file).read()
+
+
+def second_star(sequence):
+    return
+
+
+def first_star(sequence):
+    start = 0
+    size = 4
+    for i, char in enumerate(sequence):
+        subset = set(sequence[i:i+size])
+        if len(subset) == size:
+            start = i+size
+            break
+    return start
+
+
+if __name__ == "__main__":
+    problem = get_input('input.txt')
+    print("First star:", first_star(problem))
+    print("Second star:", second_star(problem))
