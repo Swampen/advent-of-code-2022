3ea06932afa9dfeb388be46c3c77a919511f8a18
Swampen <ole-ma@outlook.com>
2022-12-09 07:23:45 +0100

    👷

--- day09/ape.py
+++ day09/ape.py
@@ -1,13 +1,43 @@
+import copy
+
 def get_input(file):
     return [line.split(" ") for line in open(file).read().splitlines()]
 
 
 def second_star(moves):
-    return
+    tailPos = (0, 0)
+    tailPlaces = [tailPos]
+    ropePos = [(0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0)]
+    ropePreviousPos = [(0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0)]
+    x, y = 0, 0
+    for move in moves:
+
+        for i in range(int(move[1])):
+            print(ropePreviousPos)
+            print(ropePos)
+            print()
+            headPreviousPos = (x, y)
+            ropePreviousPos = copy.deepcopy(ropePos)
+            match move[0]:
+                case "R":
+                    x += 1
+                case "L":
+                    x -= 1
+                case "U":
+                    y += 1
+                case "D":
+                    y -= 1
+            ropePos[0] = (x, y)
+            for i, knot in enumerate(ropePos):
+                if i == 0: continue
+                if abs(ropePos[i - 1][0] - knot[0]) > 1 or abs(ropePos[i - 1][1] - knot[1]) > 1:
+                    ropePos[i] = ropePreviousPos[i - 1]
+                    if i == 9 and knot not in tailPlaces:
+                        tailPlaces.append(tailPos)
+    return len(tailPlaces)
 
 
 def first_star(moves):
-    tailMoves = 1
     tailPos = (0, 0)
     tailPlaces = [tailPos]
     x, y = 0, 0
@@ -31,6 +61,6 @@ def first_star(moves):
 
 
 if __name__ == "__main__":
-    problem = get_input('input.txt')
+    problem = get_input('test.txt')
     print("First star:", first_star(problem))
     print("Second star:", second_star(problem))
