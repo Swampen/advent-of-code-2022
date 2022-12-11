e9b4a23533a7ab00f0fa2dcc1ffecb0a87260a39
Swampen <ole-ma@outlook.com>
2022-12-11 06:59:20 +0100

    ✨

--- day10/ape.py
+++ day10/ape.py
@@ -2,7 +2,30 @@ def get_input(file):
     return [line.split(" ") for line in open(file).read().splitlines()]
 
 
+def print_pixels(dictionary):
+    for key, value in dictionary.items():
+        if (key - 1) % 40 == 0:
+            print()
+        if (key - 1) % 40 in (value - 1, value, value + 1):
+            print("#", end="")
+        else:
+            print(".", end="")
+
+
 def second_star(operations):
+    registerValue = 1
+    cycle = 0
+    s = {}
+    for opt in operations:
+        cycle += 1
+        s[cycle] = registerValue
+        if opt[0] == "noop":
+            continue
+        cycle += 1
+        s[cycle] = registerValue
+        registerValue += int(opt[1])
+    print_pixels(s)
+    print()
     return
 
 
@@ -28,6 +51,6 @@ def first_star(operations):
 
 
 if __name__ == "__main__":
-    problem = get_input('test.txt')
+    problem = get_input('input.txt')
     print("First star:", first_star(problem))
     print("Second star:", second_star(problem))
