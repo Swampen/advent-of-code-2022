7f957fcb594c1fe54878fc67ecef7808c6425315
Swampen <ole-ma@outlook.com>
2022-12-21 16:17:43 +0100

    ✨

--- day20/ape.py
+++ day20/ape.py
@@ -3,26 +3,31 @@ from utils.CircularLinkedList.CircularLinkedList import CircularLinkedList, Node
 
 
 def get_input(file):
-    return CircularLinkedList([(i, int(line)) for i, line in enumerate(open(file).read().splitlines())])
+    return [(i, int(line)) for i, line in enumerate(open(file).read().splitlines())]
 
 
 def second_star(sequence):
-    return
+    decryptionKey = 811589153
+    for node in sequence:
+        node.data = (node.data[0], node.data[1] * decryptionKey)
+    return first_star(sequence, 10)
 
 
-def first_star(sequence):
+def first_star(sequence, rounds=1):
     sum = 0
     i = 0
-    currentIndex = 0
-    while currentIndex != len(sequence):
-        index = 0
-        for node in sequence:
-            if node.data[0] == currentIndex:
-                sequence.remove(index)
-                sequence.insert(node.data, (index+node.data[1]) % len(sequence))
-                currentIndex += 1
-                break
-            index += 1
+    while i != rounds:
+        currentIndex = 0
+        while currentIndex != len(sequence):
+            index = 0
+            for node in sequence:
+                if node.data[0] == currentIndex:
+                    sequence.remove(index)
+                    sequence.insert(node.data, (index + node.data[1]) % len(sequence))
+                    currentIndex += 1
+                    break
+                index += 1
+        i += 1
     i = 0
     node = sequence.head
     while node.data[1] != 0:
@@ -37,5 +42,5 @@ def first_star(sequence):
 
 if __name__ == "__main__":
     problem = get_input("input.txt")
-    print("First star:", first_star(problem))
-    print("Second star:", second_star(problem))
+    print("First star:", first_star(CircularLinkedList(copy.deepcopy(problem))))
+    print("Second star:", second_star(CircularLinkedList(problem)))
